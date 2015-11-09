3d881263e23ced967af6664100e83ecfc0ea2864
kunhuang <963043433@qq.com>
2015-11-09 10:07:16 +0800

    搭建框架

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by lanou on 11/9/15.
+//  Copyright © 2015 黄坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by lanou on 11/9/15.
+//  Copyright © 2015 黄坤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
